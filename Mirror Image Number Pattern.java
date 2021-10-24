146eb8ddd691dc407fb8d88746715f9e8a394c08
Varun R <86638829+Varun1223@users.noreply.github.com>
2021-10-24 13:40:48 +0530

    Create Mirror Image Number Pattern.java

---
+++ Mirror Image Number Pattern.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+		/* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner s = new Scanner(System.in);
+        int n= s.nextInt();
+        int i=1;
+        while(i<=n)
+        {
+            int spaces=1;
+            while (spaces<=n-i)
+            {
+                System.out.print(" ");
+                spaces = spaces+1;
+            }
+            int numbers=1;
+            while (numbers<=i)
+            {
+                System.out.print(numbers);
+                    numbers = numbers+1;
+            }
+            System.out.println();
+            i++;
+        }
+	}
+
+}
