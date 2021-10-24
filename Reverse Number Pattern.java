78f95deee7168f53ddd5e411278f636a6754014e
Varun R <86638829+Varun1223@users.noreply.github.com>
2021-10-24 13:36:06 +0530

    Create Reverse Number Pattern.java

---
+++ Reverse Number Pattern.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Solution {
+
+
+    public static void main(String[] args) {
+
+        /* Your class should be named Solution.
+	 	* Read input as specified in the question.
+	 	* Print output as specified in the question.
+		*/
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int i=1;
+        while (i<=n)
+        {	
+            int j=1;
+            int p=i;
+            while(j<=i)
+            {
+
+                System.out.print(p);
+                p=p-1;
+                j=j+1;
+            }
+            System.out.println();
+            i+=1;
+
+        }
+
+    }
+
+}
